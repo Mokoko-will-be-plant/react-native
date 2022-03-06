ecdfe08eefb64e3c94a1f3e4f5ea4b3bbeb777bf
WJ951009 <fsdjin159@gmail.com>
2022-03-06 19:12:50 +0900

    feat: 스택 네비게이션 설정

--- POD/App.tsx
+++ POD/App.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import * as React from 'react';
+import {NavigationContainer, ParamListBase} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {Text, TouchableHighlight, View} from 'react-native';
+import {useCallback} from 'react';
+const Stack = createNativeStackNavigator();
 
-const App: React.FC = () => {
-  return (
-    <>
-      <View>
-        <Text>Hello, World</Text>
-      </View>
-    </>
-  );
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
 };
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
+
+const HomeScreen = ({navigation}: HomeScreenProps) => {
+  const onClick = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>Home Screen</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const DetailsScreen = ({navigation}: DetailsScreenProps) => {
+  const onClick = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>Details Screen</Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{title: 'Overview'}}
+        />
+        <Stack.Screen name="Details">
+          {props => <DetailsScreen {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
