3a1a1e8d0b61efcaa1acda1eaa73a3a47f9138fb
WJ951009 <fsdjin159@gmail.com>
2022-03-07 00:54:42 +0900

    feat: 중첩라우팅 구현

--- POD/src/pages/Settings.tsx
+++ POD/src/pages/Settings.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import {Pressable, View, Text} from "react-native";
 
 const Settings = () => {
+  const [count, setCount] = useState(1);
   return (
-    <></>
+    <View>
+      <Pressable onPress={(p) => setCount(p => p + 1)}><Text>{count}</Text></Pressable>
+    </View>
   )
 }
 
