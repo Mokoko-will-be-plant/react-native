ce83863de231039c35128ba6f7b5227dd5331416
WJ951009 <fsdjin159@gmail.com>
2022-03-07 00:41:27 +0900

    feat: 프로젝트 페이지 구상 & 라우트 설정

---
+++ POD/src/pages/Delivery.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Delivery = () => {
+  return (
+    <></>
+  )
+}
+
+export default Delivery;
---
+++ POD/src/pages/Ing.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Ing = () => {
+  return (
+    <></>
+  )
+}
+
+export default Ing;
