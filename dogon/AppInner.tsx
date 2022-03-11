a28df53b6941c505fc54787c431f8a3d0cbd69ae
PsmD <tbvjeld7@naver.com>
2022-03-11 23:45:04 +0900

    feat: 419에러 처리 기능 구현

--- dogon/AppInner.tsx
+++ dogon/AppInner.tsx
@@ -11,11 +11,12 @@ import {RootState} from './src/store/reducer';
 import useSocket from './src/hooks/useSocket';
 import {useEffect} from 'react';
 import EncryptedStorage from 'react-native-encrypted-storage';
-import axios, { AxiosError } from 'axios';
-import Config from 'react-native-config';
-import { Alert } from 'react-native';
-import orderSlice from './src/slices/order';
+import axios, {AxiosError} from 'axios';
+import {Alert} from 'react-native';
 import userSlice from './src/slices/user';
+import {useAppDispatch} from './src/store';
+import Config from 'react-native-config';
+import orderSlice from './src/slices/order';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -33,10 +34,45 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function AppInner() {
+  const dispatch = useAppDispatch();
   const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   console.log('isLoggedIn', isLoggedIn);
 
   const [socket, disconnect] = useSocket();
+
+  // 앱 실행 시 토큰 있으면 로그인하는 코드
+  useEffect(() => {
+    const getTokenAndRefresh = async () => {
+      try {
+        const token = await EncryptedStorage.getItem('refreshToken');
+        if (!token) {
+          return;
+        }
+        const response = await axios.post(
+          `${Config.API_URL}/refreshToken`,
+          {},
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        dispatch(
+          userSlice.actions.setUser({
+            name: response.data.data.name,
+            email: response.data.data.email,
+            accessToken: response.data.data.accessToken,
+          }),
+        );
+      } catch (error) {
+        console.error(error);
+        if ((error as AxiosError).response?.data.code === 'expired') {
+          Alert.alert('알림', '다시 로그인 해주세요.');
+        }
+      }
+    };
+    getTokenAndRefresh();
+  }, [dispatch]);
 
   useEffect(() => {
     const callback = (data: any) => {
@@ -52,7 +88,7 @@ function AppInner() {
         socket.off('order', callback);
       }
     };
-  }, [isLoggedIn, socket]);
+  }, [dispatch, isLoggedIn, socket]);
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -61,39 +97,37 @@ function AppInner() {
     }
   }, [isLoggedIn, disconnect]);
 
-    // 앱 실행 시 토큰 있으면 로그인하는 코드
-    useEffect(() => {
-      const getTokenAndRefresh = async () => {
-        try {
-          const token = await EncryptedStorage.getItem('refreshToken');
-          if (!token) {
-            return;
-          }
-          const response = await axios.post(
-            `${Config.API_URL}/refreshToken`,
-            {},
-            {
-              headers: {
-                authorization: `Bearer ${token}`,
-              },
-            },
-          );
-          dispatch(
-            userSlice.actions.setUser({
-              name: response.data.data.name,
-              email: response.data.data.email,
-              accessToken: response.data.data.accessToken,
-            }),
-          );
-        } catch (error) {
-          console.error(error);
-          if ((error as AxiosError).response?.data.code === 'expired') {
-            Alert.alert('알림', '다시 로그인 해주세요.');
+  useEffect(() => {
+    axios.interceptors.response.use(
+      response => {
+        return response;
+      },
+      async error => {
+        const {
+          config,
+          response: {status},
+        } = error;
+        if (status === 419) {
+          if (error.response.data.code === 'expired') {
+            const originalRequest = config;
+            const refreshToken = await EncryptedStorage.getItem('refreshToken');
+            // token refresh 요청
+            const {data} = await axios.post(
+              `${Config.API_URL}/refreshToken`, // token refresh api
+              {},
+              {headers: {authorization: `Bearer ${refreshToken}`}},
+            );
+            // 새로운 토큰 저장
+            dispatch(userSlice.actions.setAccessToken(data.data.accessToken));
+            originalRequest.headers.authorization = `Bearer ${data.data.accessToken}`;
+            // 419로 요청 실패했던 요청 새로운 토큰으로 재요청
+            return axios(originalRequest);
           }
         }
-      };
-      getTokenAndRefresh();
-    }, [dispatch]);
+        return Promise.reject(error);
+      },
+    );
+  }, [dispatch]);
 
   return isLoggedIn ? (
     <Tab.Navigator>
