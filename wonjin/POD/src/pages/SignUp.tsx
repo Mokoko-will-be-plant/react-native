01be2820f1a1da1dea10fa045220fe550ecc654b
wonjin-dev <fsdjin159@gmail.com>
2022-03-12 13:34:50 +0900

    feat: Axios요청 & Indicator 생성

--- wonjin/POD/src/pages/SignUp.tsx
+++ wonjin/POD/src/pages/SignUp.tsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useRef, useState} from 'react';
 import {
+  ActivityIndicator,
   Alert,
   Platform,
   Pressable,
@@ -9,12 +10,14 @@ import {
   View,
 } from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import axios, {AxiosError} from 'axios';
 import {RootStackParamList} from '../../App';
 import DismissKeyboardView from '../components/DismissKeyboardView';
 
 type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
 
 function SignUp({navigation}: SignUpScreenProps) {
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
@@ -31,7 +34,10 @@ function SignUp({navigation}: SignUpScreenProps) {
   const onChangePassword = useCallback(text => {
     setPassword(text.trim());
   }, []);
-  const onSubmit = useCallback(() => {
+  const onSubmit = useCallback(async() => {
+    if(loading){
+      return;
+    }
     if (!email || !email.trim()) {
       return Alert.alert('알림', '이메일을 입력해주세요.');
     }
@@ -55,8 +61,24 @@ function SignUp({navigation}: SignUpScreenProps) {
       );
     }
     console.log(email, name, password);
-    Alert.alert('알림', '회원가입 되었습니다.');
-  }, [email, name, password]);
+    
+    try {
+      setLoading(true);
+      const response = await axios.post('/user', {
+        email,
+        name,
+        password
+      });
+      Alert.alert('알림', '회원가입 되었습니다.');
+    } catch (err) {
+      const errorRes = (err as AxiosError).response
+      if(errorRes){
+        Alert.alert('알림', `회원가입에 실패하였습니다 :: ${errorRes.data.response}`);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, [loading, email, name, password]);
 
   const canGoNext = email && name && password;
   return (
@@ -117,9 +139,16 @@ function SignUp({navigation}: SignUpScreenProps) {
               ? StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
               : styles.loginButton
           }
-          disabled={!canGoNext}
+          disabled={!canGoNext || loading}
           onPress={onSubmit}>
-          <Text style={styles.loginButtonText}>회원가입</Text>
+            {loading
+              ? (
+                <ActivityIndicator />
+              )
+              : (
+                <Text style={styles.loginButtonText}>회원가입</Text>
+              )   
+            }
         </Pressable>
       </View>
     </DismissKeyboardView>
