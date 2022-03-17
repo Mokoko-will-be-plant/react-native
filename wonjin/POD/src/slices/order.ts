66873a093b8bd273808d46871a96faa45a059496
wonjin-dev <fsdjin159@gmail.com>
2022-03-17 23:49:56 +0900

    DAY 11: 주문 리듀서 생성

--- wonjin/POD/src/slices/order.ts
+++ wonjin/POD/src/slices/order.ts
@@ -1,6 +1,24 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
-const initialState = {
+export interface Order {
+    orderId: string;
+    start: {
+        latitude: number;
+        longitude: number;
+    };
+    end: {
+        latitude: number;
+        longitude: number;
+    };
+    price: number;
+}
+
+interface InitialState {
+    orders: Order[];
+    deliveries: Order[];
+}
+
+const initialState: InitialState = {
     orders: [],
     deliveries: []
 };
@@ -9,12 +27,27 @@ const orderSlice = createSlice({
     name: 'order',
     initialState,
     reducers: {
-        addOrder(state, action) {
+        addOrder(state, action: PayloadAction<Order>) {
             state.orders.push(action.payload);
         },
-        acceptOrder(state, action) {
+        acceptOrder(state, action: PayloadAction<String>) {
+            const index = state.orders.findIndex(v => v.orderId === action.payload);
+            if(index > -1){
+                state.deliveries.push(state.orders[index]);
+                state.orders.splice(index, 1);
+            }
         },
         rejectOrder(state, action) {
+            const index = state.orders.findIndex(v => v.orderId === action.payload);
+            if(index > -1) {
+                state.orders.splice(index, 1);
+            }
+            const delivery = state.deliveries.findIndex(
+                v => v.orderId === action.payload
+            )
+            if(delivery > -1) {
+                state.deliveries.splice(delivery, 1)
+            }
         }
     },
     extraReducers: () => {
