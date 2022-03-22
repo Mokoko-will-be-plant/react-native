2dbc85de208a7972744f524fc120e72f29a090e9
PsmD <tbvjeld7@naver.com>
2022-03-22 22:56:43 +0900

    feat: 사용자 로그인 검사 기능 추가

--- dogon/src/hooks/useSocket.ts
+++ dogon/src/hooks/useSocket.ts
@@ -1,16 +1,22 @@
 import {useCallback} from 'react';
-import {io, Socket} from 'socket.io-client';
+import SocketIOClient, {Socket} from 'socket.io-client';
+import Config from 'react-native-config';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/reducer';
 
 let socket: Socket | undefined;
 const useSocket = (): [Socket | undefined, () => void] => {
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
   const disconnect = useCallback(() => {
-    if (socket) {
+    if (socket && !isLoggedIn) {
+      console.log(socket && !isLoggedIn, '웹소켓 연결을 해제합니다.');
       socket.disconnect();
       socket = undefined;
     }
-  }, []);
-  if (!socket) {
-    socket = io('10.0.2.2:3105', {
+  }, [isLoggedIn]);
+  if (!socket && isLoggedIn) {
+    console.log(!socket && isLoggedIn, '웹소켓 연결을 진행합니다.');
+    socket = SocketIOClient(`${Config.API_URL}`, {
       transports: ['websocket'],
     });
   }
